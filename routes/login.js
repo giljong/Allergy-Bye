d807459c4d21c8b24f328bc82c802ecc0bd45977
giljong <minjong5054@daum.net>
2020-07-05 22:12:28 +0900

    bootstrap download

---
+++ routes/login.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db/connection');
+
+router.get('/', (req, res) => {
+    if(req.session.user !== undefined){
+        res.send('<script type="text/javascript">alert("이미 로그인 되어있습니다.");window.location.href="/";</script>')
+    }
+    res.render('login');
+})
+.post('/',(req, res) =>{
+    const {
+        id,pw
+    } = req.body;
+    if(req.session.user !== undefined){
+        res.send('<script type="text/javascript">alert("이미 로그인 되어있습니다.");window.location.href="/";</script>')
+    }
+    db.query('select * from Users where ID=? and PW=?',[id,pw],(err,result) =>{
+        if(err) console.log(err);
+        if(result.length===0){
+             res.send('<script type="text/javascript">alert("로그인 실패");window.location.href="/login";</script>')
+        }
+        else{
+            req.session.user = {
+                id,
+                flag : result[0].FLAG
+            }
+            req.session.save(() =>{
+                res.send('<script type="text/javascript">alert("로그인 성공");window.location.href="/";</script>')
+            })
+        }
+    })
+
+})
+
+module.exports = router;
